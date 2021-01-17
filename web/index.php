5a6f300eb7f911bdf22463b91395472b8bdf1621
bourn404 <bourn404@gmail.com>
2021-01-16 18:04:16 -0700

    added template files

--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
 <?php
 
-  phpinfo();
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php'; ?>
+  <title>Carson Fairbourn | Web Developer</title>
+</head>
+<body>
+  
+</body>
+</html>
